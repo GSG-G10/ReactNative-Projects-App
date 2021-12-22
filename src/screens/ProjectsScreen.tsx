aab5cec80775dc940fd09efca2cd597c0de7c991
muathkhatib <mkhatib.dev@gmail.com>
2021-12-22 16:23:45 +0200

    Create the first file structure Relates issue #2

---
+++ src/screens/ProjectsScreen.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable import/namespace */
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
+
+const ProjectsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ProjectsScreen</Text>
+    </View>
+  );
+}
+
+
+export default ProjectsScreen
