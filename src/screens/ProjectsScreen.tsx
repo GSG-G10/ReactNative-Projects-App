adaaedef0e5bf988d014d905194bb34e6bb81cf3
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-23 22:15:46 +0200

    fix errors Relates #3

--- src/screens/ProjectsScreen.tsx
+++ src/screens/ProjectsScreen.tsx
@@ -1,35 +1,53 @@
 /* eslint-disable import/namespace */
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
+import { View } from "../components/Themed";
+import { Card } from "../design/Card";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f5f5fa",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
 
 const ProjectsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ProjectsScreen</Text>
+      <Card
+        title="card"
+        price="2523 $"
+        icon="clockcircleo"
+        status="In Progress"
+        onPress={() => console.log("hello card")}
+      />
+      <Card
+        title="card"
+        price="2523 $"
+        icon="closecircleo"
+        status="Cancelled"
+        onPress={() => console.log("hello card")}
+      />
+      <Card
+        title="card"
+        price="2523 $"
+        icon="closecircleo"
+        status="Cancelled"
+        onPress={() => console.log("hello card")}
+      />
     </View>
   );
-}
+};
 
-
-export default ProjectsScreen
+export default ProjectsScreen;
